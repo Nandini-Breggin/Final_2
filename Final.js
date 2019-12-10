4148c2abbd01acfdaa269c5bb10e29e54ecfbeae
Nandini Breggin <nandinibreggin@gmail.com>
2019-12-09 22:03:14 -0700

    Committed

--- Final.js
+++
@@ -1,2 +0,0 @@
-// document.body.style.background="rgb(100,149,237)";
-// document.body.style.background=url("https://www.pinclipart.com/picdir/middle/157-1576395_open-racing-stripe-png-clipart.png")
